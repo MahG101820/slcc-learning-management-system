334e139404cca8c52a2eb64a13b8496e1a0c20f4
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2023-10-13 01:22:56 +0800

    update navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <TheNavbar v-if="navbarVisibility" />
+  <TheNavbar />
 
   <main class="p-4 grid place-items-center">
     <RouterView />
@@ -7,15 +7,7 @@
 </template>
 
 <script setup>
-import { ref, watch } from "vue";
-import { RouterView, useRoute } from "vue-router";
+import { RouterView } from "vue-router";
 
 import TheNavbar from "@/components/TheNavbar.vue";
-
-const route = useRoute();
-const navbarVisibility = ref();
-
-watch(route, (route) => {
-  navbarVisibility.value = route.meta.requiresAuthentication;
-});
 </script>
--- src/views/quizzes/IndexView.vue
+++ src/views/quizzes/IndexView.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Quizzes!</h1>
+  <h1 class="text-3xl font-bold capitalize underline">{{ page }}!</h1>
 </template>
+
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const page = route.name;
+</script>
